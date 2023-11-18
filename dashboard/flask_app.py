55c0e914794eb9dc9d0ea58cb442407c3cb03697
PRJM1999 <patrickmontgomery1@hotmail.co.uk>
2023-11-18 11:52:18 +0000

    UI improvements and file organisation

--- dashboard/flask_app.py
+++ dashboard/flask_app.py
@@ -1,5 +1,7 @@
 import uuid
 import json
+from datetime import datetime
+
 
 import pandas as pd
 
@@ -17,18 +19,23 @@ app.secret_key = 'your_secret_key1'
 @app.route('/home')
 def index():
     return render_template('home.html')
-
-@app.route('/manual')
-def manual():
-    return render_template('home.html')
     
 @app.route('/map')
-def map():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('home.html')
+def show_maps():
+    
+    
+    path_properties = THIS_FOLDER / './database/box_properties.csv'
+    path_data = THIS_FOLDER / './database/box_data.csv'
+    
+    box_properties = pd.read_csv(path_properties).to_dict('records')
+    box_inpections = pd.read_csv(path_data).to_dict('records')
+    
+    for box in box_properties:
+        filtered_inspections = [r for r in box_inpections if r['Section_Name'] == box['Section_Name']]
+        box['inspections'] = sorted(filtered_inspections, key=lambda d: datetime.strptime(d['Date'], "%d/%m/%Y"))
+        
+    
+    return render_template('map.html', boxes=box_properties)
 
 if __name__ == '__main__':
     app.run(debug=True)
